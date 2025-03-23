f1b94865bd015aad9464f3667eb7169760ad6036
Joseph Porter <josephporter200@gmail.com>
2025-03-23 04:37:44 -0400

    Add Pinia for state management and implement auth store

---
+++ Vue-Project.Client/src/stores/auth.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia';
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null,
+  }),
+  actions: {
+    login(user) {
+      this.user = user;
+    },
+    logout() {
+      this.user = null;
+    },
+  },
+  getters: {
+    isLoggedIn() {
+      return !!this.user;
+    },
+  },
+});
